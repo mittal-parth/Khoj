f4357cd88a5bd372df18be78fd0634fe9f3587fc
Shubham-Rasal <bluequbits@gmail.com>
2024-12-07 05:20:33 +0530

    chore: update  nft contract

--- contracts/ETHuntNFT.sol
+++ contracts/ETHuntNFT.sol
@@ -2,24 +2,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ETHuntNFT is ERC721URIStorage, Ownable {
+contract ETHuntNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("ETHunt NFT", "HUNT") {}
+    constructor() ERC721("ETHuntNFT", "EHT") {}
 
-    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
-        _tokenIds.increment();
+    function mintNFT(address player, string memory tokenURI)
+        public
+        returns (uint256)
+    {
         uint256 newItemId = _tokenIds.current();
-
-        _mint(recipient, newItemId);
+        _mint(player, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
+        _tokenIds.increment();
         return newItemId;
     }
 }
